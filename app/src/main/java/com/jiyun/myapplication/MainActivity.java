82e2a537a56c49cefb7e2da5c02f6dffb64090c0
ASUS <1959996018@qq.com>
2017-10-18 19:10:34 +0800

    一次提交

--- app/src/main/java/com/jiyun/myapplication/MainActivity.java
+++ app/src/main/java/com/jiyun/myapplication/MainActivity.java
@@ -1,20 +1,23 @@
 package com.jiyun.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tv_text)
+    TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
 
-            }
-        });
     }
 }
